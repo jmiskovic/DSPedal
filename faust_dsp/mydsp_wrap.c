f5dd9c9841082a903e12c8787227dd580086cfb6
Josip <josipmiskovic@gmail.com>
2015-01-14 00:44:57 +0100

    Fixed audio levels, kicked generated file out of repo.

--- faust_dsp/mydsp_wrap.c
+++ faust_dsp/mydsp_wrap.c
@@ -1,5 +1,15 @@
 #include "CUI.h"
 #include <fastmath.h>
 
+ #define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+ #define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 /* UGLY insertation of faust-generated source code */
 #include "mydsp.c"
